6e50588198d8f7363b7e55c7e3e4f8d7933d8756
lyula <sacredlyula@gmail.com>
2025-11-10 23:48:15 +0300

    centred the otp boxes

--- client/src/pages/RegisterCustom.tsx
+++ client/src/pages/RegisterCustom.tsx
@@ -319,7 +319,7 @@ const RegisterCustom = () => {
           {otpSent && (
             <div>
               <label className="block text-sm font-medium mb-2">Enter OTP</label>
-              <div className="flex gap-2 mb-2">
+              <div className="flex gap-2 mb-2 justify-center">
                 {[0,1,2,3,4,5].map((i) => (
                   <input
                     key={i}
